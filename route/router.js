6cf1cb0693ce820d4ce0907f4384470b1f79da07
Villarma Ryan <ryanvillarma148@gmail.com>
2024-11-16 09:38:56 +0800

    added routes for notes

--- route/router.js
+++ route/router.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import AuthController from "../controller/AuthController.js";
+import NotesController from "../controller/NotesController.js";
 import Validation from "../validation/Validation.js";
+import CookieJWTAuth from '../middleware/CookieJWTMiddleware.js';
 
 export const router = Router();
 
 router.post("/auth/register", Validation.Register(),  AuthController.Register);
 router.post("/auth/login", Validation.Login(),  AuthController.Login);
+
+router.get('/notes', CookieJWTAuth, NotesController.index);
+router.post('/notes/create', [CookieJWTAuth, Validation.Notes()], NotesController.create);
+router.patch('/notes/:id', CookieJWTAuth, NotesController.update);
+router.delete('/notes/:id', CookieJWTAuth, NotesController.destroy);
