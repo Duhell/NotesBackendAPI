e933ebdb6ee285d8892658ab3bd0f072f10b024c
Villarma Ryan <ryanvillarma148@gmail.com>
2024-11-19 02:23:15 +0800

    setting cors credential to true

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ import {router} from './route/router.js';
 const port = 3000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
