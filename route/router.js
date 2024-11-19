3429d812a5fb7d90f6d03226e10c702a69e5a16f
Villarma Ryan <ryanvillarma148@gmail.com>
2024-11-19 15:37:06 +0800

    Changed: instead of cookie, using token header auth

--- route/router.js
+++ route/router.js
@@ -2,14 +2,14 @@ import { Router } from "express";
 import AuthController from "../app/controller/AuthController.js";
 import NotesController from "../app/controller/NotesController.js";
 import Validation from "../utils/validation/Validation.js";
-import CookieJWTAuth from '../app/middleware/CookieJWTMiddleware.js';
+import BearerTokenAuth from '../app/middleware/TokenAuthMiddleware.js'
 
 export const router = Router();
 
 router.post("/auth/register", Validation.Register(),  AuthController.Register);
 router.post("/auth/login", Validation.Login(),  AuthController.Login);
 
-router.get('/notes/:id', CookieJWTAuth, NotesController.index);
-router.post('/notes/create', [CookieJWTAuth, Validation.Notes()], NotesController.create);
-router.patch('/notes/:id', [CookieJWTAuth, Validation.Notes()], NotesController.update);
-router.delete('/notes/:id', CookieJWTAuth, NotesController.destroy);
+router.get('/notes/:id', BearerTokenAuth, NotesController.index);
+router.post('/notes/create', [BearerTokenAuth, Validation.Notes()], NotesController.create);
+router.patch('/notes/:id', [BearerTokenAuth, Validation.Notes()], NotesController.update);
+router.delete('/notes/:id', BearerTokenAuth, NotesController.destroy);
