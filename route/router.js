edd368978f2ce60112d4a115e77be50bb1c67381
Villarma Ryan <ryanvillarma148@gmail.com>
2024-11-20 21:03:09 +0800

    Added: get route for all notes

--- route/router.js
+++ route/router.js
@@ -9,6 +9,7 @@ export const router = Router();
 router.post("/auth/register", Validation.Register(),  AuthController.Register);
 router.post("/auth/login", Validation.Login(),  AuthController.Login);
 
+router.get('/notes/', BearerTokenAuth, NotesController.index);
 router.get('/notes/:id', BearerTokenAuth, NotesController.index);
 router.post('/notes/create', [BearerTokenAuth, Validation.Notes()], NotesController.create);
 router.patch('/notes/:id', [BearerTokenAuth, Validation.Notes()], NotesController.update);
