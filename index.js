bbb7cffc845eaf6cb062261c2f8de2fbbd23f35c
Villarma Ryan <ryanvillarma148@gmail.com>
2024-12-06 23:27:37 +0800

    Update: added limit option

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const port = 3000;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit:'10mb'}));
+app.use(express.urlencoded({limit:'10mb', extended: true }));
 app.use(cookieParser());
 app.use('/api',router);
 
