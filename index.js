9d2bcdd975b00c55fb55f99426a5ab0401a9460e
Villarma Ryan <ryanvillarma148@gmail.com>
2024-12-07 23:41:44 +0800

    Update: removed the limit and added env port

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import {router} from './route/router.js';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
-app.use(express.json({limit:'10mb'}));
-app.use(express.urlencoded({limit:'10mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({extended: true }));
 app.use(cookieParser());
 app.use('/api',router);
 
