e20309bffb6877e3ca75cb57cd7af0811cbb2d5e
Villarma Ryan <ryanvillarma148@gmail.com>
2024-12-20 21:49:06 +0800

    Update: new routes for users getters

--- route/router.js
+++ route/router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import AuthController from "../app/controller/AuthController.js";
 import NotesController from "../app/controller/NotesController.js";
+import UserController from "../app/controller/UserController.js";
 import Validation from "../utils/validation/Validation.js";
 import BearerTokenAuth from '../app/middleware/TokenAuthMiddleware.js'
 import {uploadMulterImage} from "../app/middleware/MulterMiddleware.js"
@@ -17,5 +18,7 @@ router.post('/notes/create', [BearerTokenAuth,uploadMulterImage.single('image'),
 router.patch('/notes/:id', [BearerTokenAuth, Validation.Notes()], NotesController.update);
 router.delete('/notes/:id', BearerTokenAuth, NotesController.destroy);
 
+router.get('/users/',BearerTokenAuth, UserController.getAllUsers);
+router.get('/users/active',BearerTokenAuth, UserController.getActiveUsers);
 
 router.post("/auth/logout",BearerTokenAuth,  AuthController.Logout);
