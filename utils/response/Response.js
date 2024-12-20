d271129b41328c06ebd864b5cbad37f427f2d4b9
Villarma Ryan <ryanvillarma148@gmail.com>
2024-12-20 13:34:33 +0800

    Update: added new status code

--- utils/response/Response.js
+++ utils/response/Response.js
@@ -1,6 +1,7 @@
 export default class Response {
     static SUCCESS = 200;
     static CREATED = 201;
+    static BAD_REQUEST = 400;
     static UNAUTHORIZED = 401;
     static FORBIDDEN = 403;
     static NOT_FOUND = 404;
