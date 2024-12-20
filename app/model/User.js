7c0ad0963f2772e9cf14539f5391f3b357518a87
Villarma Ryan <ryanvillarma148@gmail.com>
2024-12-20 13:36:02 +0800

    Update: added new function for logout

--- app/model/User.js
+++ app/model/User.js
@@ -1,5 +1,7 @@
 import Model from "./Model.js";
 import bcrypt from "bcrypt";
+import { ObjectId } from "mongodb";
+
 
 export default class User extends Model {
 
@@ -10,9 +12,10 @@ export default class User extends Model {
         if (!user) return false;
         const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
         if (!isPasswordValid) return false;
+        await this.updateOne({_id: user._id},{isOnline: true},{upsert: true});
         return user;
     }
-    
+
     static async register(data){
         const hashedPassword = await bcrypt.hash(data.password, 10);
         return await this.insertOne({
@@ -20,4 +23,11 @@ export default class User extends Model {
             password: hashedPassword
         });
     }
+
+    static async logout(req){
+        const user = await this.updateOne({_id: ObjectId.createFromHexString(req.body.id) },{isOnline: false});
+        if(!user) return false;
+        return true;
+    }
+
 }
